80eba110e0f89f5a1b059ae4ed83ca166cab11b7
Pete <p.wallroth@yahoo.com>
2023-03-28 19:08:53 +1100

    Initial Commit

---
+++ Gmail Status/Gmail_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gmail_StatusApp.swift
+//  Gmail Status
+//
+//  Created by Peter Wallroth on 28/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Gmail_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
